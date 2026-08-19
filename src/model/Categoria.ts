188edb576d5faab22b9a3dc101ff448646d3bc86
Gabriel Henrique Francisco <gfrancisco.dev@gmail.com>
2026-08-19 14:30:34 -0300

    feat: adicionando métodos estáticos para listar, cadastrar e atualizar categorias no banco de dados

--- src/model/Categoria.ts
+++ src/model/Categoria.ts
@@ -1,3 +1,9 @@
+import type CategoriaDTO from "../dto/CategoriaDTO.js";
+
+import { DatabaseModel } from "./DatabaseModel.js";
+
+const database = new DatabaseModel().pool;
+
 class Categoria {
     private id_categoria: number = 0;
     private nome: string;
@@ -8,11 +14,11 @@ class Categoria {
 
     // ==================== GETTERS E SETTERS ====================
 
-    public setId(_id_categoria: number): void {
+    public setIdCategoria(_id_categoria: number): void {
         this.id_categoria = _id_categoria;
     }
 
-    public getId(): number {
+    public getIdCategoria(): number {
         return this.id_categoria;
     }
 
@@ -26,4 +32,94 @@ class Categoria {
 
     // ==================== MÉTODOS ESTÁTICOS (operações no banco de dados) ====================
 
+    static async listarCategorias(): Promise<Array<CategoriaDTO> | null> {
+
+        let listaDeCategorias: Array<CategoriaDTO> = [];
+
+        try {
+            const querySelectCategoria = `SELECT * FROM categoria;`;
+            const respostaBD = await database.query(querySelectCategoria);
+            respostaBD.rows.forEach((categoria: any) => {
+
+                const categoriaDTO: CategoriaDTO = {
+                    id_categoria: categoria.id_categoria,
+                    nome: categoria.nome
+                };
+
+                listaDeCategorias.push(categoriaDTO);
+            });
+
+            return listaDeCategorias;
+        } catch (error) {
+            console.log(`Erro ao realizar consulta: ${error}`);
+            return null;
+        }
+    }
+
+    static async listarCategoria(id_categoria: number): Promise<CategoriaDTO | null> {
+        try {
+            const querySelectCategoria = `SELECT * FROM categoria WHERE id_categoria = $1`;
+            const respostaBD = await database.query(querySelectCategoria, [id_categoria]);
+
+            const categoriaDTO: CategoriaDTO = {
+                id_categoria: respostaBD.rows[0].id_categoria,
+                nome: respostaBD.rows[0].nome
+            };
+
+            return categoriaDTO;
+        } catch (error) {
+            console.log(`Erro ao realizar a consulta: ${error}`);
+            return null;
+        }
+    }
+
+     static async cadastrarCategoria(categoria: Categoria): Promise<boolean> {
+        try {
+           
+            const queryInsertCategoria = `INSERT INTO categoria (nome)
+                                VALUES ($1) RETURNING id_categoria;`;
+
+            const result = await database.query(queryInsertCategoria, [categoria.getNome()]);
+
+            if (result.rows.length > 0) {
+                console.log(`Categoria cadastrada com sucesso. ID: ${result.rows[0].id_categoria}`);
+                return true;
+            }
+
+            return false;
+        } catch (error) {
+            console.error(`Erro ao cadastrar categoria: ${error}`);
+            return false;
+        }
+    }
+
+    static async atualizarCategoria(categoria: Categoria): Promise<boolean> {
+        try {
+            const categoriaConsulta: CategoriaDTO | null = await this.listarCategoria(categoria.id_categoria);
+
+            if (categoriaConsulta) {
+                
+                const queryAtualizarCategoria = `UPDATE categoria SET 
+                                                    nome = $1                                            
+                                                WHERE id_categoria = $2`;
+
+                
+                const respostaBD = await database.query(queryAtualizarCategoria, [
+                    categoria.getNome().toUpperCase(),       // Nome em maiúsculas
+                    categoria.id_categoria                           // ID da categoria (para o WHERE)
+                ]);
+
+                
+                
+            }
+
+            return true;
+        } catch (error) {
+            console.log(`Erro ao atualizar categoria: ${error}`);
+            throw error;
+        }
+    }
+
 }
+
+export default Categoria;
